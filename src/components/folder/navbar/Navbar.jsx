9d40b810c52d0a1786173f7f9f5260fa5ef9f0ef
Bashir Abidemi <abidemibashir.dev@gmail.com>
2026-01-25 23:45:33 +0100

    bug fixed

--- src/components/folder/navbar/Navbar.jsx
+++ src/components/folder/navbar/Navbar.jsx
@@ -9,7 +9,15 @@ const Navbar = async() => {
    console.log()
   return (
     <div className={styles.container }>
-      <Link href="/" className={styles.logo}>Bide<span>X</span>blog</Link>
+      <Link href="/" className={styles.logo}>
+        <div className={styles.logoIcon}>
+          <span className={styles.iconText}>BX</span>
+          <div className={styles.iconGlow}></div>
+        </div>
+        <div className={styles.logoText}>
+          Bide<span className={styles.highlight}>X</span>blog
+        </div>
+      </Link>
       <div>
         <Links />
       </div>
